33bf5e2ff539815d2afb4d8f9cea19e0d2667784
Nicolas Zuasti <nicozuasti@gmail.com>
2013-07-06 09:48:30 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paps::Application.config.secret_key_base = '199c4f6509b5a368b0ace6c63d0d846a4e4482be413f0820c03a46aab19dbe16d909cf0eccf2916e9a49e829a981a07abb338fe2ad7b34b08c657a2d49f9e0f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Paps::Application.config.session_store :cookie_store, key: '_paps_session'
